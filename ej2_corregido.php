af39fd66252687157b9a8778f298123acc464ebb
Mauro Chojrin <mauro.chojrin@leewayweb.com>
2019-06-10 12:11:24 -0300

    Reto 3

--- ej2_corregido.php
+++ ej2_corregido.php
@@ -1,2 +1,17 @@
 <?php
-$filtros=[];if(isset($_GET['importe'])){$filtros['importe']=$_GET['importe'];}if(isset($_GET['codigo'])){$filtros['codigo']=$_GET['codigo'];}
+    if (isset($_GET['p'])) {
+        $p = $_GET['p'];
+    } else {
+        $p = 1;
+    }
+    $t = '';
+    if (isset($_GET['t'])) {
+        $t = $_GET['t'];
+    } else if(isset($_GET['codigo'])) {
+        $t = $_GET['codigo'];
+    } else if(isset($_POST['TxtBuscar'])) {
+        $t = $_POST['TxtBuscar'];
+    }
+    if (!empty($_POST['TxtBuscar'])) {
+        $cadena = $valor[0];
+    }
